5eb520312a89c5fac1feb341a91a95ba1e49b924
Shoei Hiraishi <hiraishi@fillin-inc.com>
2021-01-15 18:16:39 +0900

    クラス クラスの定義まで

--- script-f.js
+++ script-f.js
@@ -8,32 +8,6 @@ const hexOfあ = codePointOfあ.toString(16);
 console.log(hexOfあ);
 console.log("\u{3042}");
 
-
-
-// // 文字列をCode Unit(16進数)の配列にして返す
-// function convertCodeUnits(str) {
-//     const codeUnits = [];
-//     for (let i = 0; i < str.length; i++) {
-//         codeUnits.push(str.charCodeAt(i).toString(16));
-//     }
-//     return codeUnits;
-// }
-// // 文字列をCode Point(16進数)の配列にして返す
-// function convertCodePoints(str) {
-//     return Array.from(str).map(char => {
-//         return char.codePointAt(0).toString(16);
-//     });
-// }
-
-// const str = "アオイ";
-// const codeUnits = convertCodeUnits(str);
-// console.log(codeUnits); // => ["30a2", "30aa", "30a4"]
-// const codePoints = convertCodePoints(str);
-// console.log(codePoints); // => ["30a2", "30aa", "30a4"]
-// 実行
-
-
-
 function convertCodeUnits(str) {
     const codeUnits = [];
     for (let i = 0; i < str.length; i++) {
@@ -227,12 +201,123 @@ fn();
         return "Hello";
     }
 }
-//即時実行関数より
+{
+    (function() {
+        var foo = "foo";
+        console.log(foo);
+    })();
+    console.log(typeof foo === "undefined");
+}
+{
+    {
+        const foo = "foo";
+        console.log(foo);
+    }
+    console.log(typeof foo === "undefined");
+}
+{
+function createCounter() {
+    let count = 0;
+    function increment() {
+        count = count + 1;
+        return count;
+    }
+    return increment;
+}
+const myCounter = createCounter();
+myCounter();
+myCounter();
+const newCounter = createCounter();
+newCounter();
+newCounter();
+myCounter();
+newCounter();
+}
+{
+    const x = 10;
 
+function printX() {
+    console.log(x);
+}
 
-
-
-//関数とthis
+function run() {
+    const x = 20;
+    printX();
+}
+run();
+}
+{
+    function printX() {
+        const x = "X";
+        console.log(x);
+    }
+    printX();
+}
+{
+    function createArray() {
+        const tempArray = [1, 2, 3];
+        return tempArray;
+    }
+    const array = createArray();
+    console.log(array);
+}
+{
+    const createCounter = () => {
+        let count = 0;
+        return function increment() {
+            count = count + 1;
+            return count;
+        };
+    };
+    const myCounter = createCounter();
+    console.log(myCounter());
+    console.log(myCounter());
+}
+{
+    const createCounter = () => {
+        let count = 0;
+        return function increment() {
+            count = count + 1;
+            return count;
+        };
+    };
+    const countUp = createCounter();
+    const newCountUp = createCounter();
+    console.log(countUp === newCountUp);
+    console.log(countUp());
+    console.log(newCountUp());
+}
+{
+    const createCounter = () => {
+        let privateCount = 0;
+        return () => {
+            privateCount++;
+            return `${privateCount}回目`;
+        };
+    };
+    const counter = createCounter();
+    console.log(counter());
+    console.log(counter());
+}
+{
+    function countUp() {
+        countUp.count = countUp.count + 1;
+        return countUp.count;
+    }
+    countUp.count = 0;
+    console.log(countUp());
+    console.log(countUp());
+}
+{
+    function countUp() {
+        countUp.count = countUp.count + 1;
+        return countUp.count;
+    }
+    countUp.count = 0;
+    console.log(countUp());
+    countUp.count = 10;
+    console.log(countUp());
+}
 {
     function fn1() {}
     const fn2 = function() {};
@@ -253,4 +338,267 @@ fn();
 };
 obj.method();
 }
-//Arrow Functionいがいの関数におけるthis
+{
+    "use strict";
+function fn1() {
+    return this;
+}
+const fn2 = function() {
+    return this;
+};
+console.log(fn1());
+console.log(fn2());
+}
+{
+    "use strict";
+function outer() {
+    console.log(this);
+    function inner() {
+        console.log(this);
+    }
+    inner();
+}
+outer();
+}
+{
+    const obj = {
+        method1: function() {
+            return this;
+        },
+        method2() {
+            return this;
+        }
+    };
+    console.log(obj.method1());
+    console.log(obj.method2());
+}
+{
+    const person = {
+        fullName: "Brendan Eich",
+        sayName: function() {
+            return this.fullName;
+        }
+    };
+    console.log(person.sayName());
+}
+{
+    const obj1 = {
+        obj2: {
+            obj3: {
+                method() {
+                    return this;
+                }
+            }
+        }
+    };
+    console.log(obj1.obj2.obj3.method() === obj1.obj2.obj3);
+}
+{
+    "use strict";
+const person = {
+    fullName: "Brendan Eich",
+    sayName: function() {
+        // `this`は呼び出し元によって異なる
+        return this.fullName;
+    }
+};
+console.log(person.sayName());
+const say = person.sayName;
+say();
+}
+{
+    "use strict";
+const say = function() {
+    return this.fullName;
+};
+say();
+}
+{
+    "use strict";
+function say(message) {
+    return `${message} ${this.fullName}！`;
+}
+const person = {
+    fullName: "Brendan Eich"
+};
+console.log(say.call(person, "こんにちは"));
+say("こんにちは");
+}
+{
+    "use strict";
+function say(message) {
+    return `${message} ${this.fullName}！`;
+}
+const person = {
+    fullName: "Brendan Eich"
+};
+console.log(say.apply(person, ["こんにちは"]));
+say("こんにちは");
+}
+{
+    function add(x, y) {
+        return x + y;
+    }
+    console.log(add.call(null, 1, 2));
+    console.log(add.apply(null, [1, 2]));
+}
+{
+    function say(message) {
+        return `${message} ${this.fullName}！`;
+    }
+    const person = {
+        fullName: "Brendan Eich"
+    };
+    const sayPerson = say.bind(person, "こんにちは");
+    console.log(sayPerson());
+}
+function say(message) {
+    return `${message} ${this.fullName}！`;
+}
+const person = {
+    fullName: "Brendan Eich"
+};
+const sayPerson = () => {
+    return say.call(person, "こんにちは");
+};
+console.log(sayPerson());
+{
+    "use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        return strings.map(function(str) {
+            return this.prefix + "-" + str;
+        });
+    }
+};
+Prefixer.prefixArray(["a", "b", "c"]);
+}
+{
+    "use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        const callback = function(str) {
+            return this.prefix + "-" + str;
+        };
+        return strings.map(callback);
+    }
+};
+Prefixer.prefixArray(["a", "b", "c"]);
+}
+{
+    "use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        const that = this;
+        return strings.map(function(str) {
+            return that.prefix + "-" + str;
+        });
+    }
+};
+const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
+console.log(prefixedStrings);
+}
+{
+    "use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        return strings.map(function(str) {
+            return this.prefix + "-" + str;
+        }, this);
+    }
+};
+const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
+console.log(prefixedStrings);
+}
+{
+    "use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        return strings.map((str) => {
+            return this.prefix + "-" + str;
+        });
+    }
+};
+const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
+console.log(prefixedStrings);
+}
+{
+    const fn = () => {
+        return this;
+    };
+    console.log(fn() === this);
+}
+{
+    "use strict";
+function outer() {
+    return () => {
+        return this;
+    };
+}
+const innerArrowFunction = outer();
+console.log(innerArrowFunction());
+}
+{
+    "use strict";
+function outer() {
+    const that = this;
+    return () => {
+        return that;
+    };
+}
+const innerArrowFunction = outer();
+console.log(innerArrowFunction());
+}
+{
+    const Prefixer = {
+        prefix: "pre",
+        prefixArray(strings) {
+            return strings.map((str) => {
+                return this.prefix + "-" + str;
+            });
+        }
+    };
+    const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
+    console.log(prefixedStrings);
+}
+{
+    const fn = () => {
+        return this;
+    };
+    console.log(fn());
+    console.log(fn.call({}));
+}
+{
+    const obj = {
+        method() {
+            const arrowFunction = () => {
+                return this;
+            };
+            return arrowFunction();
+        }
+    };
+    console.log(obj.method());
+    console.log(obj.method.call("THAT"));
+}
+{
+    class MyClass {
+        constructor() {
+
+        }
+    }
+}
+{
+const MyClass = class MyClass {
+    constructor() {}
+};
+
+const AnonymousClass = class {
+    constructor() {}
+};
+}
+
