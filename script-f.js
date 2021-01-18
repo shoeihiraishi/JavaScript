f0d6cf8a050b0cdfc6bed7960afbd7809942497d
Shoei Hiraishi <hiraishi@fillin-inc.com>
2021-01-18 17:40:13 +0900

    console.errorとスタックトレースまで

--- script-f.js
+++ script-f.js
@@ -585,6 +585,8 @@ console.log(innerArrowFunction());
     console.log(obj.method());
     console.log(obj.method.call("THAT"));
 }
+
+//クラス　　
 {
     class MyClass {
         constructor() {
@@ -601,4 +603,524 @@ const AnonymousClass = class {
     constructor() {}
 };
 }
+{
+    class MyClassA {
+    constructor() {
+//コンストラクタの処理が必要なら書く
+    }
+}
+//コンストラクタのよりが不要なら省略できる
+class MyClassB{
+}
+}
+{
+    class MyClass {
+    }
+    const myClass = new MyClass();
+    const myClassAnother = new MyClass();
+    console.log(myClass === myClassAnother);
+    console.log(myClass instanceof MyClass);
+    console.log(myClassAnother instanceof MyClass);
+}
+{
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+{
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    const point = new Point(3, 4);
+    console.log(point.x);
+    console.log(point.y);
+}
+{
+    class Point {
+        constructor(x, y) {
+            return { x, y };
+        }
+    }
+        const point = new Point(3, 4);
+    console.log(point);
+    console.log(point instanceof Point);
+}
+{
+const Point = function PointConstructor(x, y) {
+    this.x = x;
+    this.y = y;
+};
+const point = new Point(3, 4);
+}
+{
+    class Counter {
+        constructor() {
+            this.count = 0;
+        }
+        increment() {
+            this.count++;
+        }
+    }
+    const counterA = new Counter();
+    const counterB = new Counter();
+    counterA.increment();
+    console.log(counterA.count);
+    console.log(counterB.count);
+}
+{
+    class Counter {
+        constructor() {
+            this.count = 0;
+        }
+        increment() {
+            this.count++;
+        }
+    }
+    const counterA = new Counter();
+    const counterB = new Counter();
+    console.log(counterA.increment === counterB.increment);
+}
+{
+    class Counter {
+        constructor() {
+            this.count = 0;
+            this.increment = () => {
+                this.count++;
+            };
+        }
+    }
+    const counterA = new Counter();
+    const counterB = new Counter();
+    counterA.increment();
+    console.log(counterA.count); // => 1
+    console.log(counterB.count);
+}
+{
+    class Counter {
+        constructor() {
+            this.count = 0;
+            this.increment = () => {
+                this.count++;
+            };
+        }
+    }
+    const counterA = new Counter();
+    const counterB = new Counter();
+    console.log(counterA.increment !== counterB.increment);
+}
+{
+    "use strict";
+class ArrowClass {
+    constructor() {
+        this.method = () => {
+            return this;
+        };
+    }
+}
+const instance = new ArrowClass();
+const method = instance.method;
+console.log(method());
+}
+{
+    "use strict";
+class PrototypeClass {
+    method() {
+        return this;
+    };
+}
+const instance = new PrototypeClass();
+const method = instance.method;
+console.log(method());
+}
+{
+    class NumberWrapper {
+        constructor(value) {
+            this._value = value;
+        }
+        get value() {
+            console.log("getter");
+            return this._value;
+        }
+        set value(newValue) {
+            console.log("setter");
+            this._value = newValue;
+        }
+    }
+    const numberWrapper = new NumberWrapper(1);
+    console.log(numberWrapper.value);
+    numberWrapper.value = 42;
+    console.log(numberWrapper.value);
+}
+{
+    const array = [1, 2, 3, 4, 5];
+array.length = 2;
+console.log(array.join(", "));
+array.length = 5;
+console.log(array.join(", "));
+}
+{
+    class ArrayLike {
+        constructor(items = []) {
+            this._items = items;
+        }
+        get items() {
+            return this._items;
+        }
+        get length() {
+            return this._items.length;
+        }
+        set length(newLength) {
+            const currentItemLength = this.items.length;
+            if (newLength < currentItemLength) {
+                this._items = this.items.slice(0, newLength);
+            } else if (newLength > currentItemLength) {
+                this._items = this.items.concat(new Array(newLength - currentItemLength));
+            }
+        }
+    }
+    const arrayLike = new ArrayLike([1, 2, 3, 4, 5]);
+    arrayLike.length = 2;
+    console.log(arrayLike.items.join(", "));
+    arrayLike.length = 5;
+    console.log(arrayLike.items.join(", "));
+}
+{
+    class ArrayWrapper {
+        constructor(array = []) {
+            this.array = array;
+        }
+        static of(...items) {
+            return new ArrayWrapper(items);
+        }
+        get length() {
+            return this.array.length;
+        }
+    }
+    const arrayWrapperA = new ArrayWrapper([1, 2, 3]);
+    const arrayWrapperB = ArrayWrapper.of(1, 2, 3);
+    console.log(arrayWrapperA.length);
+    console.log(arrayWrapperB.length);
+}
+{
+    class ArrayWrapper {
+        constructor(array = []) {
+            this.array = array;
+        }
+        static of(...items) {
+            return new this(items);
+        }
+        get length() {
+            return this.array.length;
+        }
+    }
+    const arrayWrapper = ArrayWrapper.of(1, 2, 3);
+    console.log(arrayWrapper.length);
+}
+{
+    class ConflictClass {
+        constructor() {
+            this.method = () => {
+                console.log("インスタンスオブジェクトのメソッド");
+            };
+        }
+        method() {
+            console.log("プロトタイプのメソッド");
+        }
+    }
+    const conflict = new ConflictClass();
+    conflict.method();
+}
+{
+    class ConflictClass {
+        constructor() {
+            this.method = () => {
+                console.log("インスタンスオブジェクトのメソッド");
+            };
+        }
 
+        method() {
+            console.log("プロトタイプメソッド");
+        }
+    }
+    const conflict = new ConflictClass();
+    conflict.method();
+    delete conflict.method;
+    conflict.method();
+}
+{
+    function fn() {
+    }
+    console.log(typeof fn.prototype === "object");
+    class MyClass {
+    }
+    console.log(typeof MyClass.prototype === "object");
+}
+{
+    class MyClass {
+        method() { }
+    }
+    console.log(typeof MyClass.prototype.method === "function");
+    console.log(MyClass.prototype.constructor === MyClass);
+}
+{
+    class MyClass {
+        method() {
+            console.log("プロトタイプのメソッド");
+        }
+    }
+    const instance = new MyClass();
+    instance.method();
+}
+{
+    class MyClass {
+        method() {
+            console.log("プロトタイプのメソッド");
+        }
+    }
+    const instance = new MyClass();
+    const MyClassPrototype = Object.getPrototypeOf(instance);
+    console.log(MyClassPrototype === MyClass.prototype);
+}
+{
+    class MyClass {
+        method() {
+            console.log("プロトタイプのメソッド");
+        }
+    }
+    const instance = new MyClass();
+    instance.method();
+    const Prototype = Object.getPrototypeOf(instance);
+    console.log(instance.method === Prototype.method);
+}
+{
+    class MyClass {
+        method() {
+            console.log("プロトタイプのメソッド");
+        }
+    }
+    const instance = new MyClass();
+    if (instance.hasOwnProperty("method")) {
+        instance.method();
+    } else {
+        const prototypeObject = Object.getPrototypeOf(instance);
+        if (prototypeObject.hasOwnProperty("method")) {
+            prototypeObject.method.call(instance);
+        }
+    }
+}
+{
+    class Parent {
+        constructor(...args) {
+            console.log("Parentコンストラクタの処理", ...args);
+        }
+    }
+    class Child extends Parent {
+        constructor(...args) {
+            super(...args);
+            console.log("Childコンストラクタの処理", ...args);
+        }
+    }
+    const child = new Child("引数1", "引数2");
+}
+{
+    class Parent {}
+class Child extends Parent {}
+}
+{
+    class Parent {}
+class Child extends Parent {
+    constructor(...args) {
+        super(...args);
+    }
+}
+}
+{
+    class Parent {
+        constructor() {
+            this.name = "Parent";
+        }
+    }
+    class Child extends Parent {
+        constructor() {
+            super();
+            this.name = "Child";
+        }
+    }
+    const parent = new Parent();
+    console.log(parent.name);
+    const child = new Child();
+    console.log(child.name);
+}
+{
+    class Parent {
+        method() {
+            console.log("Parent#method");
+        }
+    }
+    class Child extends Parent {
+    }
+    const instance = new Child();
+    instance.method();
+}
+{
+    class Parent {
+        static hello() {
+            return "Hello";
+        }
+    }
+    class Child extends Parent {}
+    console.log(Child.hello());
+}
+{
+    class Parent {
+        method() {
+            console.log("Parent#method");
+        }
+    }
+    class Child extends Parent {
+        method() {
+            console.log("Child#method");
+            // `this.method()`だと自分(`this`)のmethodを呼び出して無限ループする
+            // そのため明示的に`super.method()`とParent#methodを呼び出す
+            super.method();
+        }
+    }
+    const child = new Child();
+    child.method();
+}
+{
+    class Parent {
+        static method() {
+            console.log("Parent.method");
+        }
+    }
+    class Child extends Parent {
+        static method() {
+            console.log("Child.method");
+            // `super.method()`で`Parent.method`を呼びだす
+            super.method();
+        }
+    }
+    Child.method();
+}
+{
+    class Parent {}
+class Child extends Parent {}
+
+const parent = new Parent();
+const child = new Child();
+console.log(parent instanceof Parent);
+console.log(parent instanceof Child);
+console.log(child instanceof Parent);
+console.log(child instanceof Child);
+}
+{
+    class MyArray extends Array {
+        get first() {
+            if (this.length === 0) {
+                return undefined;
+            } else {
+                return this[0];
+            }
+        }
+        get last() {
+            if (this.length === 0) {
+                return undefined;
+            } else {
+                return this[this.length - 1];
+            }
+        }
+    }
+    const array = MyArray.from([1, 2, 3, 4, 5]);
+    console.log(array.length);
+    console.log(array.first);
+    console.log(array.last);
+}
+
+//例外処理
+{
+    try {
+        console.log("try切:この行は実行されます");
+        undefinedFunction();
+    } catch (error) {
+        console.log("catch節:この行は実行されます");
+        console.log(error instanceof ReferenceError);
+        console.log(error.message);
+    } finally {
+        console.log("finally節:この行は実行されます");
+    }
+}
+try {
+    throw new Error("例外が投げられました");
+} catch (error) {
+    console.log(error.message);
+}
+function assertPositiveNumber(num) {
+    if (num < 0) {
+        throw new Error(`${num} is not positive.`);
+    }
+}
+{
+try {
+    assertPositiveNumber(-1);
+} catch (error) {
+    console.log(error instanceof Error);
+    console.log(error.message);
+}
+try {
+    throw "例外が投げられました";
+} catch (error) {
+    console.log(error);
+}
+}
+{
+try {
+    console.log(x);
+} catch (error) {
+    console.log(error instanceof ReferenceError);
+    console.log(error.name);
+    console.log(error.message);
+}
+}
+{
+    try {
+        eval("foo! bar!");
+    } catch (error) {
+        console.log(error instanceof SyntaxError);
+        console.log(error.name);
+        console.log(error.message);
+    }
+}
+{
+    try {
+        const fn = {};
+        fn();
+    } catch (error) {
+        console.log(error instanceof TypeError);
+        console.log(error.name);
+        console.log(error.message);
+    }
+}
+{
+    function reverseString(str) {
+        if (typeof str !== "string") {
+            throw new TypeError(`${str} is not a string`);
+        }
+        return Array.from(str).reverse().join("");
+    }
+    try {
+        reverseString(100);
+    } catch (error) {
+        console.log(error instanceof TypeError);
+        console.log(error.name);
+        console.log(error.message);
+    }
+}
+
+//非同期処理
